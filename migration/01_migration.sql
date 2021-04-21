53e80b43c0b8d22d09b2575dad1102b56c7781ba
Cristiano Altmann <crisaltmann@gmail.com>
2021-04-21 18:19:03 -0300

    Adicionando sql

--- migration/01_migration.sql
+++ migration/01_migration.sql
@@ -1,17 +1,34 @@
 CREATE TABLE IF NOT EXISTS ATIVO (
-id 			bigint 			PRIMARY KEY,
-codigo 		varchar(10) 	UNIQUE NOT NULL,
-nome 		varchar(60) 	NOT NULL
+    id 			bigint 			PRIMARY KEY,
+    codigo 		varchar(10) 	UNIQUE NOT NULL,
+    nome 		varchar(60) 	NOT NULL
 )
 
-INSERT INTO youasholding.dbo.ATIVO
+INSERT INTO ATIVO
     (codigo, nome)
 VALUES('WEGE3', 'WEGE');
 
-INSERT INTO youasholding.dbo.ATIVO
+INSERT INTO ATIVO
     (codigo, nome)
 VALUES('ITUB3', 'ITAU');
 
-INSERT INTO youasholding.dbo.ATIVO
+INSERT INTO ATIVO
     (codigo, nome)
 VALUES('HYPE3', 'HYPERA');
+
+CREATE TABLE IF NOT EXISTS USERS (
+    id          bigint          PRIMARY KEY,
+    username    varchar(10)     NOT NULL,
+    password    varchar(60)     NOT NULL
+)
+
+-- CREATE TABLE IF NOT EXISTS PORTFOLIO (
+--     id          bigint          PRIMARY KEY,
+--     user_id     bigint          NOT NULL,
+--     nome        varchar(100) NOT NULL,
+--     CONSTRAINT PORTFOLIO_FK FOREIGN KEY (id) REFERENCES youasholding.dbo.USERS(id)
+-- )
+--
+-- INSERT INTO USERS (username, password) VALUES ('teste', 'teste')
+--
+-- INSERT INTO PORTFOLIO (user_id, nome) VALUES ((SELECT id FROM USERS WHERE username = 'teste') ,'PADRÃO');
