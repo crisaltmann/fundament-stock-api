bec137a6e5363239b8c47007d43ace0444ce83be
Cristiano Altmann <crisaltmann@gmail.com>
2021-04-30 23:37:43 -0300

    change database config

--- infrastructure/database.go
+++ infrastructure/database.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/crisaltmann/fundament-stock-api/config"
 	"go.uber.org/fx"
+	"os"
 )
 
 var Module = fx.Options(
@@ -16,18 +17,14 @@ var factories = fx.Provide(
 )
 
 func CreateConnection(config *config.Config) *sql.DB {
-	//scheme := "youasholding"
-	//username := "youasholding"
-	//password := "Test123%"
-	//hostname := "youasholding.database.windows.net"
-	//port := 1433
-
 	fmt.Println("conectando base ...")
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;database=%s;port=%d",
-		config.Hostname, config.User, config.Password, config.Scheme, config.Port)
-	fmt.Println("init connection")
-	db, err := sql.Open("sqlserver", connString)
+
+	//connString := fmt.Sprintf("server=%s;user id=%s;password=%s;database=%s;port=%d",
+	//	config.Hostname, config.User, config.Password, config.Scheme, config.Port)
+	//fmt.Println("init connection")
+	//db, err := sql.Open("sqlserver", connString)
 
 	if err != nil {
 		fmt.Errorf("Erro ao contectar no banco #{err}")
