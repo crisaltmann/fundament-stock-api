bf47957bba7e28eff9ef5894ccf4e73e5afa7d8b
Cristiano Altmann <crisaltmann@gmail.com>
2021-05-01 00:52:30 -0300

    correção driver postgre

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/crisaltmann/fundament-stock-api
 go 1.15
 
 require (
-	github.com/denisenkom/go-mssqldb v0.10.0
 	github.com/gin-contrib/logger v0.0.3
 	github.com/gin-gonic/gin v1.6.3
+	github.com/lib/pq v1.10.1
 	github.com/rs/zerolog v1.18.0
 	go.uber.org/fx v1.13.1
 	gopkg.in/yaml.v2 v2.2.8
--- infrastructure/database.go
+++ infrastructure/database.go
@@ -3,16 +3,18 @@ package infrastructure
 import (
 	"database/sql"
 	"github.com/crisaltmann/fundament-stock-api/config"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 )
 
 func CreateConnection(config *config.Config) *sql.DB {
 	log.Println("conectando base ...")
+
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 
 	if err != nil {
-		log.Fatal("Erro ao contectar no banco #{err}")
+		log.Fatal("Erro ao contectar no banco", err)
 		panic(err)
 	}
 	log.Println("Connected")
