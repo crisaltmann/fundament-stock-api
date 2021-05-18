88eeeef384368aefdef50758aaabe6a9f05e233f
Cristiano Altmann <crisaltmann@gmail.com>
2021-05-18 00:13:20 -0300

    removendo código comentado.

--- order/api/converter.go
+++ order/api/converter.go
@@ -2,26 +2,6 @@ package order_api
 
 import order_domain "github.com/crisaltmann/fundament-stock-api/order/domain"
 
-//func convertToDtos(assets []domain.Asset) []AssetResponse {
-//	assetDtos := make([]AssetResponse, 0)
-//	for _, asset := range assets {
-//		assetDtos = append(assetDtos, convertDomainToDto(asset))
-//	}
-//	return assetDtos
-//}
-//
-//func convertDomainToDto(asset domain.Asset) AssetResponse {
-//	return AssetResponse{asset.Id, asset.Codigo, asset.Nome, asset.Logo}
-//}
-//
 func convertPostRequestToDomain(request OrderPostRequest) order_domain.Order {
 	return order_domain.Order{Ativo: request.Ativo, Quantidade: request.Quantidade, Valor: request.Valor}
 }
-//
-//func convertPutRequestToDomain(request AssetPutRequest, id string) (domain.Asset, error) {
-//	idInt, err := strconv.ParseInt(id, 10, 64)
-//	if err != nil {
-//		return domain.Asset{}, fmt.Errorf("Id informado é inválido.")
-//	}
-//	return domain.Asset{Id: idInt, Codigo: request.Codigo, Nome: request.Nome, Logo: request.Logo}, nil
-//}
--- order/api/handler.go
+++ order/api/handler.go
@@ -12,14 +12,6 @@ type Handler struct {
 	Service *order_service.Service
 }
 
-//func (h Handler) GetAllAssets(c *gin.Context) {
-//	assets, err := h.Service.GetAllAssets()
-//	if err != nil {
-//		c.AbortWithError(http.StatusInternalServerError, err)
-//	}
-//	c.JSON(http.StatusOK, convertToDtos(assets))
-//}
-//
 func (h Handler) InsertOrder(c *gin.Context) {
 	order := OrderPostRequest{}
 	c.BindJSON(&order)
@@ -29,30 +21,3 @@ func (h Handler) InsertOrder(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, nil)
 }
-//
-//func (h Handler) UpdateAsset(c *gin.Context) {
-//	asset := AssetPutRequest{}
-//	c.BindJSON(&asset)
-//	domainAsset, err := convertPutRequestToDomain(asset, c.Param("id"))
-//	if err != nil {
-//		c.AbortWithError(http.StatusBadRequest, err)
-//	}
-//	rasset, err := h.Service.UpdateAsset(domainAsset)
-//	if err != nil {
-//		c.AbortWithError(http.StatusInternalServerError, err)
-//	}
-//	c.JSON(http.StatusOK, rasset)
-//}
-//
-//func (h Handler) GetById(c *gin.Context) {
-//	idParam := c.Param("id")
-//	id, err := strconv.ParseInt(idParam, 10, 64)
-//	if err != nil {
-//		c.AbortWithError(http.StatusBadRequest, err)
-//	}
-//	asset, err := h.Service.GetById(id)
-//	if err != nil {
-//		c.AbortWithError(http.StatusInternalServerError, err)
-//	}
-//	c.JSON(http.StatusOK, asset)
-//}
