faa086dd727b453879828b3a9d69b22a70c77756
Cristiano Altmann <crisaltmann@gmail.com>
2021-05-18 22:50:00 -0300

    Add swagger

--- asset/api/router.go
+++ asset/api/router.go
@@ -2,6 +2,20 @@ package asset_api
 
 import "github.com/crisaltmann/fundament-stock-api/server"
 
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server asset server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host another.swagger.io
+// @BasePath /v2
 func MapRouter(server *server.Server, handler *Handler) {
 	server.Server.GET(Path, handler.GetAllAssets)
 	server.Server.POST(Path, handler.InsertAsset)
