13b362f38154f92d3cec84682a262be055aa54b9
Cristiano Altmann <cristiano.altmann@mercadolivre.com>
2021-06-21 21:38:31 -0300

    Add importer.

--- pkg/result_importer/api/handler.go
+++ pkg/result_importer/api/handler.go
@@ -16,9 +16,18 @@ func NewHandler(importer result_importer_service.Importer) Handler {
 	return Handler{importer: importer}
 }
 
+func (h Handler) ImportAll(c *gin.Context) {
+	err := h.importer.StartImporterAll()
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, nil)
+}
 
 func (h Handler) Import(c *gin.Context) {
-	err := h.importer.StartImporter("")
+	code := c.Query("code")
+	err := h.importer.StartImporter(code)
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
--- pkg/result_importer/api/router.go
+++ pkg/result_importer/api/router.go
@@ -4,4 +4,5 @@ import "github.com/crisaltmann/fundament-stock-api/server"
 
 func MapRouter(server *server.Server, handler Handler) {
 	server.Server.POST(Path, handler.Import)
+	server.Server.POST(Path+"-all", handler.ImportAll)
 }
